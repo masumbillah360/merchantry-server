1eec6b909ccc0c6578e321224482224ed4b9525f
Masum Billah <masumbillah6584@gmail.com>
2022-11-28 03:43:14 +0600

    added route for delete user

--- index.js
+++ index.js
@@ -105,6 +105,12 @@ const dbRunner = async () => {
       const result = await userCollection.updateOne(filter, updatedDoc);
       res.send(result);
     });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(filter);
+      res.send(result);
+    });
     app.get("/categories", async (req, res) => {
       const query = {};
       const result = await categoryCollection.find(query).toArray();
