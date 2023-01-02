fe7aa4adb04cdfcc13c33915a5ce2fb3eda1cbd5
Masum Billah <masumbillah6584@gmail.com>
2023-01-02 17:39:03 +0600

    added route on index page

--- files/handler/orderHandler.js
+++ files/handler/orderHandler.js
@@ -1,13 +1,19 @@
-app.get("/orders", verifyJWT, async (req, res) => {
+const express = require("express");
+const { paymentCollection } = require("../db_collections/collection");
+const router = express.Router();
+
+router.get("/", verifyJWT, async (req, res) => {
   const email = req.query.email;
   console.log(email);
   const query = { userEmail: email };
   const result = await paymentCollection.find(query).toArray();
   res.send(result);
 });
-app.delete("/orders/:id", verifyJWT, async (req, res) => {
+router.delete("/:id", verifyJWT, async (req, res) => {
   const id = req.params.id;
   const query = { _id: ObjectId(id) };
   const result = await paymentCollection.deleteOne(query);
   res.send(result);
 });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const usersHandler = require("./files/handler/userHandler");
+const bookingHandler = require("./files/handler/bookingHandler");
+const productsHandler = require("./files/handler/productsHandler");
+const categoriesHandler = require("./files/handler/categoryHandler");
 
 const app = express();
 // configure file
@@ -14,7 +18,14 @@ app.use(express.json());
 
 const apiRunner = async () => {
   try {
+    // users route
     app.use("/users", usersHandler);
+    // products handler
+    app.use("/products", productsHandler);
+    // category handler
+    app.use("/categories", categoriesHandler);
+    // booking route
+    app.use("/booking", bookingHandler);
   } catch (error) {}
 };
 
